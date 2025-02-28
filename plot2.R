95f2ead122c106d48095ef6144d83cce7da99c2a
Ghizlane Kabboul <ghizlane.kabboul@gmail.com>
2025-02-28 02:16:39 +0000

    commit

---
+++ plot2.R
@@ -0,0 +1,19 @@
+# 1- Download and load the dataset:
+setwd("C:/Users/dell/OneDrive/Bureau/Coursera/Johns_Hopkins_university/C4_Exploratory Data Analysis/Semaine 1/Course_Project/ExData_Plotting1")
+fileurl <-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileurl, destfile = "./projectdataset.zip")
+unzip("./projectdataset.zip")
+data<- read.table("household_power_consumption.txt", sep=";", header = TRUE)
+library(dplyr)
+data2<-data %>%
+  filter(Date %in% c("1/2/2007","2/2/2007"))
+data2[,(3:9)]<-data.frame(sapply(data2[(3:9)], as.numeric))
+
+# 2- Convert the Date and Time variables to Date/Time classes 
+data2$DateTime<-paste(data2$Date, data2$Time)
+data2$DateTime<-strptime(data2$DateTime, format="%d/%m/%Y %H:%M:%S")
+
+#Plot2
+plot(data2$DateTime, data2$Global_active_power, type="l", xlab="", ylab ="Global Active Power (Kilowatts)")
+dev.copy(png, file="Plot2.png", width=480, height=480, units="px")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,22 @@
+# 1- Download and load the dataset:
+setwd("C:/Users/dell/OneDrive/Bureau/Coursera/Johns_Hopkins_university/C4_Exploratory Data Analysis/Semaine 1/Course_Project/ExData_Plotting1")
+fileurl <-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileurl, destfile = "./projectdataset.zip")
+unzip("./projectdataset.zip")
+data<- read.table("household_power_consumption.txt", sep=";", header = TRUE)
+library(dplyr)
+data2<-data %>%
+  filter(Date %in% c("1/2/2007","2/2/2007"))
+data2[,(3:9)]<-data.frame(sapply(data2[(3:9)], as.numeric))
+
+# 2- Convert the Date and Time variables to Date/Time classes 
+data2$DateTime<-paste(data2$Date, data2$Time)
+data2$DateTime<-strptime(data2$DateTime, format="%d/%m/%Y %H:%M:%S")
+
+#Plot3
+plot(data2$DateTime, data2$Sub_metering_1, type="l", col="black", xlab="", ylab ="Energy sub metering")
+lines(data2$DateTime, data2$Sub_metering_2, col="red")
+lines(data2$DateTime, data2$Sub_metering_3, col="blue")
+legend("topright", col=c("black", "red", "blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lwd=1)
+dev.copy(png, file="Plot3.png", width=480, height=480, units="px")
+dev.off()
